1bef3c5bc53ef8589cec92a1e7124d28d0533371
sota <tanuma321bre@gmail.com>
2020-02-13 21:50:51 +0900

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title></title>
+<meta charset="utf-8">
+</head>
+<body>
+<p>aa</p>
+<script>
+document.write("が大きい");
+</script>
+</body>
+</html>
